6fdd72c91426db4f4e7848c6fa6b890f78aceea0
magvtv <magutujr@gmail.com>
2026-02-23 14:07:34 +0300

    refactor: update application routing to support new components

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GadgetListComponent } from './features/gadget-list/gadget-list.component';
+import { GadgetFormComponent } from './features/gadget-form/gadget-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GadgetListComponent },
+  { path: 'new', component: GadgetFormComponent },
+  { path: 'edit/:id', component: GadgetFormComponent },
+  { path: '**', redirectTo: '' }
+];
